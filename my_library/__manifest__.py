64f816df3718769810879e60d4253bc733b27b50
Muhammed Nasser Shalaby Alazaly <alazaly@outlook.com>
2022-03-25 14:21:51 +0200

    part1

---
+++ my_library/__manifest__.py
@@ -0,0 +1,11 @@
+{
+ 'name': "My library",
+ 'summary': "Manage books easily",
+ 'author': "Muhammad Nasser",
+ 'version': '13.0.1',
+ 'depends': ['base'],
+ 'data': ['security/groups.xml',
+          'security/ir.model.access.csv',
+          'views/library_book.xml',
+          'views/library_book_categ.xml'],
+}
---
+++ my_library/models/library_book.py
@@ -0,0 +1,96 @@
+from odoo import models, fields, api
+from datetime import timedelta
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book'
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name'
+    short_name = fields.Char('Short Title', required=True)
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection(
+        [('draft', 'Not Available'),
+         ('available', 'Available'),
+         ('lost', 'Lost')],
+        'State')
+    description = fields.Html('Description')
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
+    date_release = fields.Date('Release Date')
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer('Number of Pages')
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        digits=(14, 4),  # Optional precision decimals,
+    )
+    cost_price = fields.Float(
+        'Book Cost', digits='Book Price')
+    currency_id = fields.Many2one(
+        'res.currency', string='Currency')
+    retail_price = fields.Monetary(
+        'Retail Price',
+        # optional: currency_field='currency_id',
+    )
+    name = fields.Char('Title', required=True)
+    date_release = fields.Date('Release Date')
+    author_ids = fields.Many2many(
+        'res.partner',
+        string='Authors'
+        )
+    publisher_id = fields.Many2one(
+        'res.partner', string='Publisher',
+        # optional:
+        ondelete='set null',
+        context={},
+        domain=[],
+    )
+    category_id = fields.Many2one('library.book.category')
+    age_days = fields.Float(
+        string='Days Since Release',
+        compute='_compute_age',
+        inverse='_inverse_age',
+        search='_search_age',
+        store=False,  # optional
+        compute_sudo=True  # optional
+    )
+
+    @api.depends('date_release')
+    def _compute_age(self):
+        today = fields.Date.today()
+
+        for book in self:
+            if book.date_release:
+                delta = today - book.date_release
+                book.age_days = delta.days
+            else:
+                book.age_days = 0
+
+    def _inverse_age(self):
+        today = fields.Date.today()
+        for book in self.filtered('date_release'):
+            d = today - timedelta(days=book.age_days)
+            book.date_release = d
+
+    def _search_age(self, operator, value):
+        today = fields.Date.today()
+        value_days = timedelta(days=value)
+        value_date = today - value_days
+        # convert the operator:
+        # book with age > value have a date < value_date
+        operator_map = {
+                            '>': '<', '>=': '<=',
+                            '<': '>', '<=': '>=',
+                        }
+        new_op = operator_map.get(operator, operator)
+        return [('date_release', new_op, value_date)]
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    published_book_ids = fields.One2many('library.book', 'publisher_id', string='Published Books')
+    authored_book_ids = fields.Many2many(
+        'library.book',
+        string='Authored Books',
+        # relation='library_book_res_partner_rel' #optional
+    )
